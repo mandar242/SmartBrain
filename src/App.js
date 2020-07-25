272c89d2e4b2756fa1b32a8651dbd7df408db705
sharvarikhedkar <sharvari996@gmail.com>
2020-07-25 16:42:36 -0700

    Signin, Register, Rank route connection complete

--- src/App.js
+++ src/App.js
@@ -37,8 +37,25 @@ class App extends Component {
       imageUrl:'',
       box: {},
       route:'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user:{
+            id:'',
+            name:'',
+            email: '',
+            entries: 0,
+            joined: ''
+      }
     }
+  }
+
+  loadUser = (data) =>{
+    this.setState({user:{
+            id:data.id,
+            name:data.name,
+            email: data.email,
+            entries: data.entries,
+            joined: data.joined
+    }})
   }
 
   calcuateFaceLocation = (data) => {
@@ -112,15 +129,15 @@ class App extends Component {
           ? <div>
               
               <Logo />
-              <Rank/>
+              <Rank name={this.state.user.name} entries = {this.state.user.entries}/>
               <ImageLinkForm onInputChange= {this.onInputChange} onButtonSubmit ={this.onButtonSubmit} /> 
               <FaceRecognition box = {this.state.box} imageUrl={imageUrl}/>  
             </div>
           
           : (
             this.state.route === 'signin'
-            ? <Signin onRouteChange={this.onRouteChange}/> 
-            : <Register onRouteChange={this.onRouteChange}/>
+            ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/> 
+            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
             )
        
       }
