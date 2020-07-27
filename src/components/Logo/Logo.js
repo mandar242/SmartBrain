0616912d8c20e69cf9495170b405596cafdf3f64
sharvarikhedkar <sharvari996@gmail.com>
2020-07-26 17:52:29 -0700

    signin issue solved

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -7,7 +7,7 @@ const Logo = () => {
     return(
         <div className = 'ma4 mt0'>
             <Tilt className="Tilt br2 shadow-2" options={{ max : 50 }} style={{ height: 80, width:90}} >
-                <div className="Tilt-inner pa3"><img stlye={{paddingTop:'15px'}} src={brain}/></div>
+                <div className="Tilt-inner pa3"><img stlye={{paddingTop:'15px'}} src={brain} alt="not found"/></div>
             </Tilt>
         </div>
     );
